08d16bf77a1dd9552706e5f409367bc2927e900a
steinermichael <michaelsteiner1985@googlemail.com>
2014-12-28 21:40:08 +0100

    -changed a few thing -backup :)

--- platforms/ios/www/js/controller/platformTests/PL_LocalStorageCtrl.js
+++ platforms/ios/www/js/controller/platformTests/PL_LocalStorageCtrl.js
@@ -15,60 +15,6 @@ sdApp.controller('PL_LocalStorageCtrl', function ($scope, $rootScope, TestHelper
         $scope.$apply();
     };
 
-    //$scope.startPlatformTest = function () {
-    //
-    //    //the function writes x-items to LocalStorage.
-    //    //after x-items the UI will be updated to show some progress for the user
-    //    //after that the function will continue
-    //    //It will continue until it reaches max quota.
-    //
-    //    function nextLoop() {
-    //
-    //        console.log('called nextLoop(' + $scope.currentIteration + ')');
-    //        var loopLength= 10000;
-    //        try {
-    //
-    //            console.log('currentIteration (before loop):' + $scope.currentIteration);
-    //            var i = $scope.currentIteration;
-    //            for (; i < ($scope.currentIteration + loopLength); i++) {
-    //
-    //                localStorage.setItem(keyPrefix + '' + TestHelperFactory.fillWithZeroes(10, i), value);
-    //
-    //            }
-    //
-    //            $scope.currentIteration = (parseInt($scope.currentIteration) + loopLength);
-    //
-    //            //without the timeouts the $apply function is not working for updating the UI
-    //            setTimeout(
-    //                function () {
-    //                    //to update the UI - gives the user an update about the progress as the test progresses
-    //                    $scope.$apply();
-    //                    setTimeout(
-    //                        nextLoop(parseInt($scope.currentIteration)), 500);
-    //                }, 1000);
-    //
-    //        } catch (e) {
-    //            if (e.name === 'QuotaExceededError') {
-    //                console.log('error is QuotaExceededError');
-    //
-    //            }
-    //
-    //            $scope.result = 'exception at ' + i;
-    //            alert($scope.result);
-    //            console.log('result:' + $scope.result);
-    //            $scope.testInProgress = false;
-    //            $scope.$apply();
-    //
-    //        }
-    //
-    //    };
-    //
-    //    //start the test
-    //    $scope.currentIteration = 0;
-    //    nextLoop();
-    //
-    //};
-
     $scope.startPlatformTest = function () {
 
         //the function writes x-items to LocalStorage.
@@ -107,7 +53,7 @@ sdApp.controller('PL_LocalStorageCtrl', function ($scope, $rootScope, TestHelper
         //start the test
 
         var datasetStringToSave = JSON.stringify(testDataFactory.getDatasetForPlatformTest());
-        $scope.testInProgress = false;
+        $scope.testInProgress = true;
         $scope.$apply();
 
         $scope.currentIteration = 1;
--- www/js/controller/platformTests/PL_LocalStorageCtrl.js
+++ www/js/controller/platformTests/PL_LocalStorageCtrl.js
@@ -9,65 +9,22 @@ sdApp.controller('PL_LocalStorageCtrl', function ($scope, $rootScope, TestHelper
     $scope.localStorage = localStorage;
 
     $scope.prepare = function () {
+
+
+
+
+
+
+
+
+
+
+
         localStorage.clear();
         $scope.isPrepared = true;
         $scope.currentIteration = '';
         $scope.$apply();
     };
-
-    //$scope.startPlatformTest = function () {
-    //
-    //    //the function writes x-items to LocalStorage.
-    //    //after x-items the UI will be updated to show some progress for the user
-    //    //after that the function will continue
-    //    //It will continue until it reaches max quota.
-    //
-    //    function nextLoop() {
-    //
-    //        console.log('called nextLoop(' + $scope.currentIteration + ')');
-    //        var loopLength= 10000;
-    //        try {
-    //
-    //            console.log('currentIteration (before loop):' + $scope.currentIteration);
-    //            var i = $scope.currentIteration;
-    //            for (; i < ($scope.currentIteration + loopLength); i++) {
-    //
-    //                localStorage.setItem(keyPrefix + '' + TestHelperFactory.fillWithZeroes(10, i), value);
-    //
-    //            }
-    //
-    //            $scope.currentIteration = (parseInt($scope.currentIteration) + loopLength);
-    //
-    //            //without the timeouts the $apply function is not working for updating the UI
-    //            setTimeout(
-    //                function () {
-    //                    //to update the UI - gives the user an update about the progress as the test progresses
-    //                    $scope.$apply();
-    //                    setTimeout(
-    //                        nextLoop(parseInt($scope.currentIteration)), 500);
-    //                }, 1000);
-    //
-    //        } catch (e) {
-    //            if (e.name === 'QuotaExceededError') {
-    //                console.log('error is QuotaExceededError');
-    //
-    //            }
-    //
-    //            $scope.result = 'exception at ' + i;
-    //            alert($scope.result);
-    //            console.log('result:' + $scope.result);
-    //            $scope.testInProgress = false;
-    //            $scope.$apply();
-    //
-    //        }
-    //
-    //    };
-    //
-    //    //start the test
-    //    $scope.currentIteration = 0;
-    //    nextLoop();
-    //
-    //};
 
     $scope.startPlatformTest = function () {
 
@@ -107,7 +64,7 @@ sdApp.controller('PL_LocalStorageCtrl', function ($scope, $rootScope, TestHelper
         //start the test
 
         var datasetStringToSave = JSON.stringify(testDataFactory.getDatasetForPlatformTest());
-        $scope.testInProgress = false;
+        $scope.testInProgress = true;
         $scope.$apply();
 
         $scope.currentIteration = 1;
